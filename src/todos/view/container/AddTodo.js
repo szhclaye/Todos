fdb19bd50234d6a0d2fdc4d0fcb3f854bf8e1d06
szhclaye <328044021@qq.com>
2018-03-31 07:53:39 +0800

    3.31

--- src/todos/view/container/AddTodo.js
+++ src/todos/view/container/AddTodo.js
@@ -1,30 +1,33 @@
-import React from "react";
-import { connect } from "react-redux";
-import  "./addTodo.css";
+import React from 'react'
+import { connect } from 'react-redux'
+import './addTodo.css'
 
-import { addTodo } from "../../actionsCreator";
+import { addTodo } from '../../actionsCreator'
+
 
 const AddTodo = ({ dispatch }) => {
-  let input;
-  return (
-    <div className="container">
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          if (!input.value.trim()) {
-            return;
-          }
-          dispatch(addTodo(input.value));
-          input.value = "";
-        }}
-      >
-        <input  className="input_i"
-                placeholder="输入待办事项.." 
-                ref={node => (input = node)} />
-        <button type="submit">添加</button>
-      </form>
-    </div>
-  );
-};
+    let input
+    return (
+        <div className="container">
+            <form
+                onSubmit={e => {
+                    e.preventDefault()
+                    if (!input.value.trim()) {
+                        return
+                    }
+                    dispatch(addTodo(input.value))
+                    input.value = ''
+                }}
+            >
+                <input
+                    className="input_i"
+                    placeholder="输入待办事项.."
+                    ref={node => (input = node)}
+                />
+                <button type="submit">添加</button>
+            </form>
+        </div>
+    )
+}
 
-export default connect()(AddTodo);
+export default connect()(AddTodo)
