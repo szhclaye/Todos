4283b90304cdf8a7e051ad943748f89e96e2295c
szhclaye <328044021@qq.com>
2018-04-02 21:59:52 +0800

    master

--- src/Store.js
+++ src/Store.js
@@ -1,4 +1,6 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+import createHistory from 'history/createBrowserHistory';
+import { routerReducer,routerMiddleware } from 'react-router-redux';  /** 引入react router */
 
 import { reducer as todoReducer } from "./todos";
 import { reducer as filterReducer } from "./filter";
@@ -10,11 +12,12 @@ win.Perf = Perf;
 
 const reducers = combineReducers({
   todos: todoReducer,
-  visibilityFilter: filterReducer
+  visibilityFilter: filterReducer,
+  router:routerReducer
 });
 // state.todos   state.visibilityFilter
 
-const middlewares = [];
+const middlewares = [routerMiddleware(history)];
 if (process.env.NODE_ENV !== "production") {
   middlewares.push(require("redux-immutable-state-invariant")());
 }
--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,19 +1,47 @@
-import React from "react";
-import { view as Todos } from "./todos";
-import { view as Filter } from "./filter";
+import React from 'react'
+import { ConnectedRouter } from 'react-router-redux';
+import { Route,Link } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+
+import { view as Todos } from './todos'
+import { view as Filter } from './filter'
 
 const styles = {
-    marginTop:'50px',
-    fontSize:'40px',
-    textAlign:'center'
+    marginTop: '50px',
+    fontSize: '40px',
+    textAlign: 'center',
 }
+const history = createHistory();
 
 const TodoApp = () => (
-  <div>
-    <h1 style={styles}>Todos</h1>
-    <Todos />
-    <Filter />
-  </div>
-);
+    <ConnectedRouter history={history}>
+        <div>
+            <h1 style={styles}>Todos</h1>
+            <Todos />
+            <Filter />
+            <ul>
+                <li><Link to='/home'>Home</Link></li>
+                <li><Link to='/about'>About</Link></li>
+                <li><Link to='/topics'>Topic</Link></li>
+            </ul>
+            <hr />
+            <Route path='/home' component={Home} />
+            <Route path='/about' component={About} />
+            <Route path='/topics' component={Topics} />
+        </div>
+    </ConnectedRouter>
+)
 
-export default TodoApp;
+const Home = () =>(
+    <div>Home</div>
+)
+
+const About = () =>(
+    <div>About</div>
+)
+
+const Topics = () =>(
+    <div>Topics</div>
+)
+
+export default TodoApp
