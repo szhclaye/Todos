c9d63a2f9bc5a765dd31173583100ba0adac1173
szhclaye <328044021@qq.com>
2018-04-02 22:37:54 +0800

    new

--- src/Store.js
+++ src/Store.js
@@ -1,43 +1,43 @@
-import { createStore, combineReducers, applyMiddleware, compose } from "redux";
-<<<<<<< HEAD
-import createHistory from 'history/createBrowserHistory';
-import { routerReducer,routerMiddleware } from 'react-router-redux';  /** 引入react router */
-=======
-import thunkMiddleware from 'redux-thunk';
->>>>>>> szhclaye
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import createHistory from 'history/createBrowserHistory'
+import {
+    routerReducer,
+    routerMiddleware,
+} from 'react-router-redux' /** 引入react router */
+import thunkMiddleware from 'redux-thunk'
+import loggerMiddleware from 'redux-logger'
 
-import { reducer as todoReducer } from "./todos";
-import { reducer as filterReducer } from "./filter";
-import { reducer as weatherReducer } from './weathers';
+import { reducer as todoReducer } from './todos'
+import { reducer as filterReducer } from './filter'
+import { reducer as weatherReducer } from './weathers'
 
-import Perf from "react-addons-perf";
+import Perf from 'react-addons-perf'
 
-const win = window;
-win.Perf = Perf;
+const win = window
+win.Perf = Perf
 
 const reducers = combineReducers({
-  todos: todoReducer,
-  visibilityFilter: filterReducer,
-<<<<<<< HEAD
-  router:routerReducer
-});
+    todos: todoReducer,
+    visibilityFilter: filterReducer,
+    weatherOfCity: weatherReducer,
+    router: routerReducer,
+})
 // state.todos   state.visibilityFilter
+const history = createHistory()
 
-const middlewares = [routerMiddleware(history)];
-=======
-  weatherOfCity:weatherReducer
-});
-// state.todos   state.visibilityFilter
-
-const middlewares = [thunkMiddleware];
->>>>>>> szhclaye
-if (process.env.NODE_ENV !== "production") {
-  middlewares.push(require("redux-immutable-state-invariant")());
+const middlewares = [
+    thunkMiddleware,
+    loggerMiddleware,
+    routerMiddleware(history)
+];
+/* thunkMiddleware要放在第一位 */
+if (process.env.NODE_ENV !== 'production') {
+    middlewares.push(require('redux-immutable-state-invariant')())
 }
 
 const storeEnhancers = compose(
-  applyMiddleware(...middlewares),
-  win && win.devToolsExtension ? win.devToolsExtension() : f => f
-);
+    applyMiddleware(...middlewares),
+    win && win.devToolsExtension ? win.devToolsExtension() : f => f,
+)
 
-export default createStore(reducers, {}, storeEnhancers);
+export default createStore(reducers, {}, storeEnhancers)
--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,64 +1,49 @@
-<<<<<<< HEAD
 import React from 'react'
-import { ConnectedRouter } from 'react-router-redux';
-import { Route,Link } from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
+import { ConnectedRouter } from 'react-router-redux'
+import { Route, Link } from 'react-router-dom'
+import createHistory from 'history/createBrowserHistory'
 
 import { view as Todos } from './todos'
 import { view as Filter } from './filter'
-=======
-import React from "react";
-import { view as Todos } from "./todos";
-import { view as Filter } from "./filter";
 import { view as Weather } from './weathers';
->>>>>>> szhclaye
 
 const styles = {
     marginTop: '50px',
     fontSize: '40px',
     textAlign: 'center',
 }
-const history = createHistory();
+const history = createHistory()
 
 const TodoApp = () => (
-<<<<<<< HEAD
     <ConnectedRouter history={history}>
         <div>
             <h1 style={styles}>Todos</h1>
             <Todos />
             <Filter />
+            <Weather />
             <ul>
-                <li><Link to='/home'>Home</Link></li>
-                <li><Link to='/about'>About</Link></li>
-                <li><Link to='/topics'>Topic</Link></li>
+                <li>
+                    <Link to="/home">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/topics">Topic</Link>
+                </li>
             </ul>
             <hr />
-            <Route path='/home' component={Home} />
-            <Route path='/about' component={About} />
-            <Route path='/topics' component={Topics} />
+            <Route path="/home" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/topics" component={Topics} />
         </div>
     </ConnectedRouter>
 )
 
-const Home = () =>(
-    <div>Home</div>
-)
+const Home = () => <div>Home</div>
 
-const About = () =>(
-    <div>About</div>
-)
+const About = () => <div>About</div>
 
-const Topics = () =>(
-    <div>Topics</div>
-)
-=======
-  <div>
-    <h1 style={styles}>Todos</h1>
-    <Todos />
-    <Filter />
-    <Weather />
-  </div>
-);
->>>>>>> szhclaye
+const Topics = () => <div>Topics</div>
 
 export default TodoApp
